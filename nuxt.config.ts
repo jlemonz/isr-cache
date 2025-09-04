a2ccd870eac06d48bdbb5a84cec9c3e8967652d9
johndoe <johndoe@email.com>
2025-09-04 11:47:33 +0200

    w

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
   routeRules: {
-    '/': {
-      isr: 300
-    }
-  }
+    // all routes (by default) will be revalidated every 60 seconds, in the background
+    '/**': { isr: 60 },
+    // this page will be generated on demand and then cached permanently
+    '/static': { isr: true },
+    // this page is generated at build time and cached permanently
+    '/prerendered': { prerender: true },
+    // this page will be always fresh
+    '/dynamic': { isr: false },
+    // you can do lots more with route rules too!
+    '/redirect': { redirect: '/static' },
+    '/headers': { headers: { 'x-magic-of': 'nuxt and vercel' } },
+    '/spa': { ssr: false },
+  },
 })
